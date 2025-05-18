705860ec57c2dfec957969b073d5579cdb306c17
Vinay Goel <54267249+goelvinay5@users.noreply.github.com>
2025-05-18 17:49:08 +0530

    added as href

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import {
-Nav,
-NavLink,
-Bars,
-NavMenu,
-NavBtn,
-NavBtnLink,
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
 } from './NavbarElements';
 
 const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<Bars />
-		<NavMenu>
-      <NavLink to='/home' activeStyle>Home</NavLink>
-	<NavLink to='https://goelvinay5.github.io/easyprogramming/' activeStyle>Easy Programming</NavLink>
-	<NavLink to='/organizer' activeStyle>Organizer</NavLink>
-      <NavLink to='/dsa' activeStyle>DSA</NavLink>
-      {/* Second Nav */}
-      {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-		</NavMenu>
-		{/* <NavBtn>
+    return (
+        <>
+            <Nav>
+                <Bars />
+                <NavMenu>
+                    <NavLink to='/home' activeStyle>Home</NavLink>
+                    <NavLink to='/easyprogramming/' activeStyle>Easy Programming</NavLink>
+                    <a href='https://goelvinay5.github.io/easyprogramming/'>Easy Programming</a>
+                    <NavLink to='/organizer' activeStyle>Organizer</NavLink>
+                    <NavLink to='/dsa' activeStyle>DSA</NavLink>
+                    {/* Second Nav */}
+                    {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+                </NavMenu>
+                {/* <NavBtn>
 		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
 		</NavBtn> */}
-	</Nav>
-	</>
-);
+            </Nav>
+        </>
+    );
 };
 
 export default Navbar;
